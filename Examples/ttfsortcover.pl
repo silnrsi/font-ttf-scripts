23732d7476659bce54586ae3f65d4aaef4ddc438
BobH <BobH@4dbf3599-f9e3-0310-9a96-835ab5cc0019>
2011-06-20 18:41:40 +0000

    added ttfsortcover.pl to Examples
    git-svn-id: https://scripts.sil.org/svn/utilities/Font-TTF-scripts/trunk@712 4dbf3599-f9e3-0310-9a96-835ab5cc0019

---
+++ Examples/ttfsortcover.pl
@@ -0,0 +1,25 @@
+#! /usr/bin/perl
+use Font::TTF::Font;
+use Font::TTF::Useall;
+use Data::Dumper;
+
+unless (defined $ARGV[1])
+{
+    die <<'EOT';
+    ttfsortcover infile outfile
+Corrects all OT lookups and GDEF for unsorted coverage tables
+EOT
+}
+
+$f = Font::TTF::Font->open($ARGV[0]) || die "Unable to open font file $ARGV[0]";
+$f->{' noharmony'} = 1;		# Don't bother harmonizing script and language tags
+
+foreach my $tag (qw(GPOS GSUB GDEF))
+{
+	next unless exists $f->{$tag};
+	$f->{$tag}->read->dirty;
+}
+
+$f->update;
+$f->out($ARGV[1]);
+exit;
