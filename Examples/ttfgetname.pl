ea47c696ef93433587c940d57b56881b1ccebb21
davidr <davidr@4dbf3599-f9e3-0310-9a96-835ab5cc0019>
2011-09-26 21:12:00 +0000

    Added ttfgetname.pl to Examples
    git-svn-id: https://scripts.sil.org/svn/utilities/Font-TTF-scripts/trunk@728 4dbf3599-f9e3-0310-9a96-835ab5cc0019

---
+++ Examples/ttfgetname.pl
@@ -0,0 +1,60 @@
+#! /usr/bin/perl
+use strict;
+use open OUT=>':utf8'; 
+use Font::TTF::Font;
+use Getopt::Std;
+
+#use Data::Dumper;
+#print Dumper($n1) , "\n";
+
+our ($opt_a,$opt_o,$VERSION);
+
+getopts('ao:');
+$VERSION = '0.1'; # original 
+
+unless ($#ARGV == 1 and $ARGV[0] =~ /^\d+$/)
+{
+    die <<"EOT";
+
+ttfgetname [-a][-o outputfile] number fontfile
+
+Extracts a string from the name table in fontfile based on number supplied and displays on screen.
+
+Options:
+
+ -o    Output to file instead of screen
+ -a    Output all values for the string with corresponding pid, eid and lid values
+
+Version $VERSION
+EOT
+}
+
+my ($number, $Font) = @ARGV;
+if ($opt_o) {
+	unless (open (STDOUT,">:utf8", $opt_o)) {die ("Could not open $opt_o for output");}
+	binmode STDOUT;
+}
+
+# Open font and read the name table
+my $f = Font::TTF::Font->open($Font) || die ("Couldn't open TTF '$Font'\n");
+my $name_table = $f->{'name'}->read;
+
+if (not $opt_a) { # Find the first copy of the string based on the find_name sub in name.pm
+	my ($name);
+	$name = $name_table->find_name($number);
+	unless ($name) {die("Could not find string in name table for id: $number\n");}
+	print $name, "\r\n";
+}
+else { # loop round finding all copies of the string
+	my $params = $name_table->{'strings'}[$number];
+	unless (ref($params)) {die("Could not find string in name table for id: $number\n");}
+	my ($pid,$eid,$lid);
+	foreach $pid (0 .. $#{$name_table->{'strings'}[$number]}) {
+ 		foreach $eid (0 .. $#{$name_table->{'strings'}[$number][$pid]}) {
+			foreach $lid (sort keys %{$name_table->{'strings'}[$number][$pid][$eid]}) {
+				print "Platform ID: $pid, Encoding ID: $eid, Language ID: $lid \r\n";
+				print $name_table->{'strings'}[$number][$pid][$eid]{$lid},"\r\n\r\n";
+			}
+		}
+	}
+}	
