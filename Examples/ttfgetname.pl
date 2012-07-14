c306270caccc0d283f9fac4c807afd375ab7de6d
BobH <BobH@4dbf3599-f9e3-0310-9a96-835ab5cc0019>
2012-07-14 02:57:58 +0000

    ttfgetname.pl: added support for multiple name IDs.
    git-svn-id: https://scripts.sil.org/svn/utilities/Font-TTF-scripts/trunk@823 4dbf3599-f9e3-0310-9a96-835ab5cc0019

--- Examples/ttfgetname.pl
+++ Examples/ttfgetname.pl
@@ -7,15 +7,18 @@ use Getopt::Std;
 our ($opt_a,$opt_o,$VERSION);
 
 getopts('ao:');
-$VERSION = '0.1'; # original 
+$VERSION = '0.2'; # BH: Added support for multiple ID numbers
+#$VERSION = '0.1'; # original 
 
-unless ($#ARGV == 1 and $ARGV[0] =~ /^\d+$/)
+unless ($#ARGV == 1 and $ARGV[0] =~ /^(?:\d|,|\.)+$/)
 {
     die <<"EOT";
 
-ttfgetname [-a][-o outputfile] number fontfile
+ttfgetname [-a][-o outputfile] number(s) fontfile
 
-Extracts a string from the name table in fontfile based on number supplied and displays on screen.
+Extracts one or more strings from the name table in fontfile based on name ID number(s) supplied.
+If a single number is supplied, program warns if the name table does not contain that string.
+Ranges and/or lists of name ID numbers may be supplied, e.g. "2..5,8,10".
 
 Options:
 
@@ -32,26 +35,36 @@ if ($opt_o) {
 	binmode STDOUT;
 }
 
+my @numbers = eval("($number,)");
+
 # Open font and read the name table
 my $f = Font::TTF::Font->open($Font) || die ("Couldn't open TTF '$Font'\n");
 my $name_table = $f->{'name'}->read;
 
-if (not $opt_a) { # Find the first copy of the string based on the find_name sub in name.pm
-	my ($name);
-	$name = $name_table->find_name($number);
-	unless ($name) {die("Could not find string in name table for id: $number\n");}
-	print $name, "\r\n";
-}
-else { # loop round finding all copies of the string
-	my $params = $name_table->{'strings'}[$number];
-	unless (ref($params)) {die("Could not find string in name table for id: $number\n");}
-	my ($pid,$eid,$lid);
-	foreach $pid (0 .. $#{$name_table->{'strings'}[$number]}) {
- 		foreach $eid (0 .. $#{$name_table->{'strings'}[$number][$pid]}) {
-			foreach $lid (sort keys %{$name_table->{'strings'}[$number][$pid][$eid]}) {
-				print "Platform ID: $pid, Encoding ID: $eid, Language ID: $lid \r\n";
-				print $name_table->{'strings'}[$number][$pid][$eid]{$lid},"\r\n\r\n";
-			}
+for $number (@numbers) {
+	print "String ID: $number\r\n" if @numbers > 1;
+	if (not $opt_a) { # Find the first copy of the string based on the find_name sub in name.pm
+		my ($name);
+		$name = $name_table->find_name($number);
+		if ($name) {
+			print $name, "\r\n";
+			next;
 		}
 	}
+	else { # loop round finding all copies of the string
+		my $params = $name_table->{'strings'}[$number];
+		if (ref($params)) {
+			my ($pid,$eid,$lid);
+			foreach $pid (0 .. $#{$name_table->{'strings'}[$number]}) {
+		 		foreach $eid (0 .. $#{$name_table->{'strings'}[$number][$pid]}) {
+					foreach $lid (sort keys %{$name_table->{'strings'}[$number][$pid][$eid]}) {
+						print "Platform ID: $pid, Encoding ID: $eid, Language ID: $lid \r\n";
+						print $name_table->{'strings'}[$number][$pid][$eid]{$lid},"\r\n\r\n";
+					}
+				}
+			}
+			next;
+		}
+	}
+	warn("Could not find string in name table for id: $number\n") unless @numbers > 1;	
 }	
