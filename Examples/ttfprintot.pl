2eb100ba7011685fe0bdb97efb7ec7143c5bdaa9
BobH <BobH@4dbf3599-f9e3-0310-9a96-835ab5cc0019>
2012-07-20 20:49:05 +0000

    ttfprintof.pl now parses VOLT source if available so it can include lookup names.
    git-svn-id: https://scripts.sil.org/svn/utilities/Font-TTF-scripts/trunk@825 4dbf3599-f9e3-0310-9a96-835ab5cc0019

--- Examples/ttfprintot.pl
+++ Examples/ttfprintot.pl
@@ -2,11 +2,13 @@
 use strict;
 use Font::TTF::Font;
 use Font::TTF::OTTags qw( %tttags %ttnames readtagsfile );
+use Font::TTF::Scripts::Volt;
+use IO::File;
 use Getopt::Std;
 use Pod::Usage;
 
-our ($opt_h, $opt_l, $opt_v);
-getopts('hlv');
+our ($opt_h, $opt_l, $opt_t, $opt_v);
+getopts('hlt:v');
 
 unless (defined $ARGV[0] || defined $opt_h)
 {
@@ -19,6 +21,18 @@ if ($opt_h)
     pod2usage( -verbose => 2, -noperldoc => 1);
     exit;
 }
+
+# See if we can find any VOLT source:
+
+if ($opt_t)
+{
+    my ($inf) = IO::File->new("< $opt_t") || die "Can't open file $opt_t";
+    $opt_t = '';
+    while (<$inf>)
+    { $opt_t .= $_; }
+    $inf->close;
+}
+
 
 my %LookupText = (
 GSUB => ['Single substitution', 'Multiple', 'Alternate', 'Ligature', 'Contextual', 'Chain contextual', 'Extension', 'Reverse Chain'],
@@ -35,13 +49,35 @@ my @FlagDetail = (
 
 foreach (@ARGV)
 {
-	my $font = Font::TTF::Font->open($_);
-	unless ($font)
+	my $if = Font::TTF::Scripts::Volt->read_font($_);
+	unless ($if)
 	{
-		print STDERR "Unable to open font file '$_'\n";
+		warn "Can't read font $_";
 		next;
 	}
+	my $font = $if->{'font'};
+	
 	print "Font file: '$_'\n" if $#ARGV > 0;
+	
+	my %lookupNames = (GSUB => [], GPOS => []);
+	
+	# See if any VOLT source:
+	my $volt_text = defined $font->{'TSIV'} ? $font->{'TSIV'}->read->{' dat'} : $opt_t;
+	if (defined ($volt_text))
+	{
+		# Parse the VOLT text and extract lookup names:
+		my $volt = $if->parse_volt($volt_text);
+		foreach my $l (@{$volt->{'lookups'}})
+		{
+			my $name = $l->{'id'};
+			# NB: In VOLT source, a series of "lookups" with names like zork\a, zork\b, zork\c represent
+			# subtables of a single actual OpenType lookup.
+			$name =~ s/\\.*$//oi;	# strip off subtable indicator if any
+			my $type = 'G' . uc($l->{'lookup'}[0]);		# gives GSUB or GPOS
+			push @{$lookupNames{$type}}, $name unless $lookupNames{$type}[-1] eq $name;
+		}
+	}
+			
 	foreach my $t (qw(GSUB GPOS))
 	{
 		next unless exists $font->{$t};
@@ -95,7 +131,9 @@ foreach (@ARGV)
 			foreach my $il (0 .. scalar(@{$g->{'LOOKUP'}})-1)
 			{
 				my $l = $g->{'LOOKUP'}[$il];
-				print "    $il: Type = $l->{'TYPE'} ($LookupText{$t}[$l->{'TYPE'}-1])";
+				print "    $il: ";
+				print "Name = $lookupNames{$t}[$il]\n        " if defined $volt_text;
+				print "Type = $l->{'TYPE'} ($LookupText{$t}[$l->{'TYPE'}-1])";
 				if ($l->{'FLAG'})
 				{
 					print " Flag = $l->{'FLAG'} (";
@@ -128,7 +166,7 @@ ttfprintot - prints the Script/Language/Feat hierarchy of OpenType files
 
 =head1 SYNOPSIS
 
-  ttfprintot [-v] infile.ttf ...
+  ttfprintot [-l] [-t volt.txt] [-v] infile.ttf ...
   ttfprintot -h
 
 Prints to STDOUT information about the Script, Language, and Feature structure of one or more OpenType font files.
@@ -136,6 +174,7 @@ Prints to STDOUT information about the Script, Language, and Feature structure o
 =head1 OPTIONS
 
   -l   enumerate Lookups as well as Scripts, Languages and Features
+  -t   Volt source file to use instead of what is in the font
   -v   include debugging entries added by Microsoft VOLT
   -h   print help message
 
@@ -160,6 +199,22 @@ This shows that there is one script (with tag "latn") and that contains both the
 language with tag "IPA ". The "->" beside the IPA language indicates that internally the DEFAULT and IPA languages
 use the same language table.  The sequences of numbers beside the Features entries give the indicies 
 of lookups that are assigned to the features. Thus the Mark Positioning feature uses lookups 0 and 1.
+
+If -l is provided, the lookups are also enumerated, e.g:
+
+  Lookups:
+    0: Name = LamAlefConnection
+        Type = 3 (Cursive) Flag = 9 (RightToLeft IgnoreMarks)
+        Subtable 0: Format = 1 Number of rules = 38
+    1: Name = MarksAbove
+        Type = 4 (Mark to base) Flag = 1 (RightToLeft)
+        Subtable 0: Format = 1 Number of rules = 828
+    2: Name = DaggerOnLam
+        Type = 6 (Mark to mark) Flag = 1 (RightToLeft)
+        Subtable 0: Format = 1 Number of rules = 57
+
+Note that lookup names will be output only if the VOLT source project is included in the font
+or supplied by -t parameter.
 
 When a font has been compiled, but not shipped, by Microsoft VOLT, the OpenType tables contain
 additional languages and features used by VOLT's Proofing Tool. These are not included
